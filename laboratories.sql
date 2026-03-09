4b22ad957363c1d9993d1d61103de7cbc598637a
Beyshin <thedemexus@gmail.com>
2026-03-09 15:37:09 +0100

    fixes

--- laboratories.sql
+++ laboratories.sql
@@ -1,10 +1,4 @@
-CREATE TABLE lab_tests(
+CREATE TABLE laboratories(
 	id INTEGER PRIMARY KEY,
-	date_of_test DATE NOT NULL,
-	description VARCHAR2(255),
-	is_valid BOOLEAN NOT NULL,
-	type VARCHAR2(20) NOT NULl,
-	astronaut_id INTEGER FOREIGN KEY REFERENCES astronauts(id),
-	doctor_id INTEGER FOREIGN KEY REFERENCES doctors(id),
-	lab_id INTEGER FOREIGN KEY REFERENCES laboratories(id)
+	address VARCHAR2(50) NOT NULL
 )
