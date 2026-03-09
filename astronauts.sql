d77022fff452566ebea96ed7cb7283b15d828c1a
Krzysztof <spyrakrzys@gmail.com>
2026-03-09 14:59:55 +0100

    test1

---
+++ astronauts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Astronauts (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    gender VARCHAR(10) CHECK (gender IN ('M', 'F', 'Other')),
+    blood_group VARCHAR(30) NOT NULL,
+    date_of_birth DATE,
+    height DECIMAL(5,2) CHECK (height > 140.00 AND height < 220.00)
+);
